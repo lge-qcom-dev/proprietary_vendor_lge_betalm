ec19180119e2d03de2b4375af61d3bc4b67ef6c4
Han Sol Jin <hansol@hansol.ca>
2024-09-12 15:01:17 -0700

    betalm: drop libsnsapi.so
    Change-Id: I41ebfa6e8cd4f7dbbfc7de87ce0857ba5e6a4eae

--- betalm-vendor.mk
+++ betalm-vendor.mk
@@ -643,7 +643,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/betalm/proprietary/vendor/lib/libmorpho_image_stab31.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmorpho_image_stab31.so \
     vendor/lge/betalm/proprietary/vendor/lib/libmorpho_wdr2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmorpho_wdr2.so \
     vendor/lge/betalm/proprietary/vendor/lib/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmpbase.so \
-    vendor/lge/betalm/proprietary/vendor/lib/libsnsapi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsnsapi.so \
     vendor/lge/betalm/proprietary/vendor/lib/libspectre.so:$(TARGET_COPY_OUT_VENDOR)/lib/libspectre.so \
     vendor/lge/betalm/proprietary/vendor/lib/libsymphony-1.1.2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsymphony-1.1.2.so \
     vendor/lge/betalm/proprietary/vendor/lib/libsymphony-1.1.4.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsymphony-1.1.4.so \
@@ -925,7 +924,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/betalm/proprietary/vendor/lib64/libmorpho_image_stab31.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmorpho_image_stab31.so \
     vendor/lge/betalm/proprietary/vendor/lib64/libmorpho_wdr2.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmorpho_wdr2.so \
     vendor/lge/betalm/proprietary/vendor/lib64/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so \
-    vendor/lge/betalm/proprietary/vendor/lib64/libsnsapi.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsnsapi.so \
     vendor/lge/betalm/proprietary/vendor/lib64/libspectre.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libspectre.so \
     vendor/lge/betalm/proprietary/vendor/lib64/libsymphony-1.1.4.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsymphony-1.1.4.so \
     vendor/lge/betalm/proprietary/vendor/lib64/libsymphony-cpu.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsymphony-cpu.so \
